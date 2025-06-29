19b807cdc64c088cd8237647bade6c7cb0f93541
salam9642 <salamshaik.outlook@gmail.com>
2025-06-29 13:30:19 +0530

    Anu added few Methods

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -2,7 +2,17 @@ package org.sam;
 
 public class Sample {
 
+	private void m1() {
+		System.out.println("Method-----1");
+	}
+	
+	
 	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.m1();
+		
+		
 		int a = 10;
 		System.out.println(a);
 
