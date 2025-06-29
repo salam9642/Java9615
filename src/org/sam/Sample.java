39c22fed4ef8e59e5d6f79714837455f5f0cdd1b
salam9642 <salamshaik.outlook@gmail.com>
2025-06-29 14:34:41 +0530

    Banu sone her code

--- src/org/sam/Sample.java
+++ src/org/sam/Sample.java
@@ -1,11 +1,15 @@
 package org.sam;
 
 public class Sample {
+	
+	public static String name = "SmartSkills";
 
 	public static void main(String[] args) {
 		int a = 10;
 		System.out.println(a);
-
+		
+		System.out.println(name);
+		
 	}
 
 }
